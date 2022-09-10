b5716529cae7305a5ecf09809bea932ba6429754
Phunbie <72875215+Phunbie@users.noreply.github.com>
2022-09-10 14:46:05 +0000

    add dynamic layout and api route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app= Flask(__name__)
 
@@ -23,6 +23,10 @@ JOBS=[{"id": 1,
 def hello():
   return render_template("home.html",jobs=JOBS,
                         first_name="Oluwafunbi")
+
+@app.route("/api/jobs")
+def listjobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",debug=True)
